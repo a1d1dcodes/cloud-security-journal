6b5f23fb7f0f51cc9b2b23d038a8757787055150
gitacc-hub <github2001acc@gmail.com>
2026-08-16 13:16:04 +0300

    Added filehandling python code

--- training/filehandling.py
+++ training/filehandling.py
@@ -1,13 +1,23 @@
+#Reading file contents
 """with open("security.log","r") as file:
     content=file.read()
     print(content)"""
 
+#Writing to a file
 """with open("names.txt","w") as file:
     content=file.write("Abdirahman Ahmed \nAyaan Ahmed \nShukri Ahmed")
     print(content)"""
 
+#Appending to a file
+"""with open("names.txt","a") as file:
+    file.write("\nAbdirahman Ahmed")"""
 
-file=r"C:\Users\user\OneDrive\Desktop\cloud-security-journey\auth.log"
-with open(file,"r") as file:
-    print(file.readline())
-    print(file.readline())
+#Streaming line by line
+"""with open("names.txt","r") as file:
+    for line in file:
+        print(line.strip())"""
+
+with open("report.txt","w") as file:
+    file.write("Bronze layer processed: 500 records\n")
+with open("report.txt","a") as file:
+    file.write("Silver layer processed: 480 records\n")
